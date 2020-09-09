7150d2c018b4909107954b6dac21f95ef8843fdf
nbutcher1 <22nbutcher@athenian.org>
2020-09-09 22:14:16 +0000

    finsihsh

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,26 +4,48 @@ public class MyMain {
     
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        int count = 0;
+        for (int i = 0; i > str.length(); i++) {
+            char oof = str.charAt(i);
+            if (oof == 'a')
+                count += 1;
+            if (oof == 'c')
+                count += 1;
+            if (oof == 'b')
+                count += 1;
+        }
+        return count;
     }
 
     // Checks to see whether or not str contains 'The' or 'the'
     public static boolean containsThe(String str) {
-        // REPLACE WITH YOUR CODE
-        return false;
+        for (i = 0; i < str.length(); i ++) {
+            if (str.charAt(i) == "t" || "T")
+                if (str.charAt(i+1) == "h")
+                    if (str.charAt(i+2) =="e")
+                        boolean the = true;
+
+
+        }
+        return the;
     }
 
     // Checks whether str is a palindrome or not
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
-        return false;
+        String palin;
+        for (i = 0; i < str.length(); i ++){
+            palin += str.charAt(str.length - i);
+        }
+        return palin == str;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println(countABC(wowzah));
+        System.out.println(containsThe(the store));
+        System.out.println(isPalindrom(racecar));
 
-        // YOUR CODE HERE
+
     }
 }
